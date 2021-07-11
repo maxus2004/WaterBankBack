45a29ce4e59dfaea3a49e6597074e062c8e70c3d
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-11 12:52:33 +0300

    update tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,10 @@
-
+def save(bottles):
+    file = open('result.json', 'w')
+    json.dump(file, bottles)
+    file.close()
+    
+def load():
+    file = open('result.json', 'r')
+    bottles = json.loads(file.read())
+    file.close()
+    return bottles
