c6edebbb019b68274804eb937336f0fd3270ac75
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-11 11:49:02 +0300

    update app.py

--- app.py
+++ app.py
@@ -4,8 +4,27 @@ from flask import request
 app = Flask(__name__)
 
 @app.route('/get')
-def index():
+def getcmd():
     location = request.args.get('id')
-    return 'hello ' + location
+    return 'n bottles in ' + location
+
+@app.route('/set')
+def setcmd():
+    location = request.args.get('id')
+    count = request.args.get('count')
+    return 'set bottles in ' + location + ' to ' + count
+    
+@app.route('/add')
+def addcmd():
+    location = request.args.get('id')
+    count = request.args.get('count')
+    return 'added ' + count + ' bottles to ' + location
+    
+@app.route('/remove')
+def addcmd():
+    location = request.args.get('id')
+    count = request.args.get('count')
+    return 'removed ' + count + ' bottles from ' + location
+    
     
 app.run(port=5000)
