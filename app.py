75a6daa93b3a8fdef6c9ec2f097fd7428beb1bab
maxus2004 <65509668+maxus2004@users.noreply.github.com>
2021-07-11 12:02:57 +0300

    update app.py

--- app.py
+++ app.py
@@ -4,28 +4,37 @@ import json
 
 app = Flask(__name__)
 
+bottles = {}
+
 @app.route('/get')
 def getcmd():
     location = request.args.get('id')
+    
     return json.dumps({"count":5})
 
 @app.route('/set')
 def setcmd():
     location = request.args.get('id')
     count = request.args.get('count')
+    bottles[location] = count;
     return json.dumps({"result":"ok"})
 
 @app.route('/add')
 def addcmd():
     location = request.args.get('id')
     count = request.args.get('count')
+    bottles[location] += count;
     return json.dumps({"result":"ok"})
     
 @app.route('/remove')
 def removecmd():
     location = request.args.get('id')
     count = request.args.get('count')
-    return json.dumps({"result":"ok"})
-    
+    if(bottles[location] >= count)
+        bottles[location] -= count
+        bottles[location])
+        return json.dumps({"result":"ok"})
+    else
+    return json.dumps({"result":"not_enough_bottles"})
     
 app.run(port=5000)
